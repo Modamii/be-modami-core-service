02e5bc8fb89b30be48d6a88b5bbbb5904eb2e9ea
Phong Nguyễn Cao <nguyencaophong.ptit@gmail.com>
2026-04-02 20:39:49 +0700

    Update API documentation to change server host references from localhost to production address in main.go, docs.go, swagger.json, and swagger.yaml.

--- docs/docs.go
+++ docs/docs.go
@@ -2716,7 +2716,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8087",
+	Host:             "modami-core.techinsightsworld.com",
 	BasePath:         "/v1/core-services",
 	Schemes:          []string{"http", "https"},
 	Title:            "Modami Core Service API",
